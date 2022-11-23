aad10fb7c651bcea8deedb5260d3b49103750b4d
Noah <noahwinzig@gmail.com>
2022-11-23 17:30:47 -0500

    working on splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -18,8 +18,39 @@ function SplashPage(){
 
 
     return (
-        <div>
+        <div className='splashWrapper'>
+            <div className='introWrapper'>
+                <div className='contentWrapper'>
+                    <img className='splashImage' src='https://res.cloudinary.com/dydhvazpw/image/upload/v1669240945/capstone/images_wtngcc.jpg' alt='book club clipart'>
+                    </img>
+                    <div>
+                        <p className='splashIntrotext'>
+                            Join or create a book club to read with old or new friends.
+                        </p>
+                    </div>
+                </div>
+                <div className='contentWrapper'>
+                    <img className='splashImage' src='https://res.cloudinary.com/dydhvazpw/image/upload/v1669240832/capstone/istockphoto-965384578-612x612_njvsuy.jpg' alt='book clipart'>
+                    </img>
+                    <div>
+                        <p className='splashIntrotext'>
+                            Find a book, or add something new to share.
+                        </p>
+                    </div>
+                </div>
+                <div className='contentWrapper'>
+                    <img className='splashImage' src='https://res.cloudinary.com/dydhvazpw/image/upload/v1669242442/capstone/istockphoto-1158861397-612x612_rhgnue.jpg' alt='reading clipart'>
+                    </img>
+                    <div>
+                        <p className='splashIntrotext'>
+                            Never lose track of what your reading and plan your next book.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className='getStartedWrapper'>
 
+            </div>
         </div>
     )
 }
